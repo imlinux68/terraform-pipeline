3674d44444d43c5d514e86589999a2655fa3bbe8
imlinux68 <imlinux68@gmail.com>
2023-12-13 10:56:13 +0200

    added rt

--- main.tf
+++ main.tf
@@ -21,6 +21,21 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_internet_gateway" "my_igw" {
 	vpc_id = aws_vpc.my_vpc.id
 	tags = {
-		Name = "My_IGW"
+		Name = "My_IGW-${random_id.my_rand.dec}"
 	}
+}
+
+
+resource "aws_route_table" "my_rt" {
+	vpc_id = aws_vpc.my_vpc.id
+	tags = {
+		Name = "MY_RT-${random_id.my_rand.dec}"
+	}
+}
+
+resource "aws_route" "my_route" {
+	route_table_id = aws_route_table.my_rt.id
+	destination_cidr_block = "0.0.0.0/0"
+	gateway_id = aws_internet_gateway.my_igw.id
+
 }
