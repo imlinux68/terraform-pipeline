68bc59f6d8bcba2fced2067c7deef9aba9ad0a5c
imlinux68 <imlinux68@gmail.com>
2023-12-13 10:20:39 +0200

    added lc

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
+
+
+resource "random_id" "my_rand" {
+	byte_length = 2
+
+}
+
 resource "aws_vpc" "my_vpc" {
 	cidr_block = var.vpc_cidr
 	enable_dns_hostnames = true
 	enable_dns_support = true
 	tags = {
-		Name = "my_vpc"
+		Name = "my_vpc-${random_id.my_rand.dec}"
+	}
+	lifecycle {
+	create_before_destroy = true
 	}
 }
 
