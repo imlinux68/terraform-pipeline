480f25da68fd24d4f0c9c5f6bf6e9711ce3691d1
imlinux68 <imlinux68@gmail.com>
2023-12-13 09:28:08 +0200

    added variables

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
-resources "aws_vpc" "my_vpc" {
-	cidr_block = "10.123.0.0/16"
+resource "aws_vpc" "my_vpc" {
+	cidr_block = var.vpc_cidr
 	enable_dns_hostnames = true
 	enable_dns_support = true
 	tags = {
-		Name = my_vpc"
+		Name = "my_vpc"
 	}
+}
+
+
+resource "aws_internet_gateway" "my_igw" {
+	vpc_id = aws_vpc.my_vpc.id
+
 }
